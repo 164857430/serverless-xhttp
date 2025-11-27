45c73c75817da6a922569306e26d377ef8f54728
CubaRafael <bibo1112@163.com>
2025-11-27 21:39:14 +0800

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const { Buffer } = require('buffer');
 const { exec } = require('child_process');
 
 // 环境变量
-const UUID = process.env.UUID || '18de1598-0e56-4e37-b4e3-7ae81ed629bd'; // 使用哪吒v1，在不同的平台部署需修改UUID，否则会覆盖
+const UUID = process.env.UUID || '18de1598-0e56-4e37-b4e3-7ae81edf29bd'; // 使用哪吒v1，在不同的平台部署需修改UUID，否则会覆盖
 const NEZHA_SERVER = process.env.NEZHA_SERVER || 'nginx.hii.pp.ua:80';       // 哪吒v1填写形式：nz.abc.com:8008   哪吒v0填写形式：nz.abc.com
 const NEZHA_PORT = process.env.NEZHA_PORT || '';           // 哪吒v1没有此变量，v0的agent端口为{443,8443,2096,2087,2083,2053}其中之一时开启tls
 const NEZHA_KEY = process.env.NEZHA_KEY || 'sSS4zjyBhxT8fmEiCyqdliHSrVvV5gze';             // v1的NZ_CLIENT_SECRET或v0的agent端口  
